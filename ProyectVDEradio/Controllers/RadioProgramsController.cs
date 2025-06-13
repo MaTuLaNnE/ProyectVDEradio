bdfbb87d1b81165e527033ba8a15a604be1134c2
MaTuLaNnE <matias.lanne@estudiantes.utec.edu.uy>
2025-06-13 01:54:13 -0300

    Arreglo de RadioProgram

---
+++ ProyectVDEradio/Controllers/RadioProgramsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectVDEradio.Models;
+using ProyectVDEradio.ViewModels;
+
+namespace ProyectVDEradio.Controllers
+{
+    public class RadioProgramsController : Controller
+    {
+        private VozDelEsteDBEntities db = new VozDelEsteDBEntities();
+
+        // GET: RadioPrograms
+        public ActionResult Index()
+        {
+            var now = DateTime.Now;
+            var currentTime = now.TimeOfDay;
+            var currentDay = (int)now.DayOfWeek;
+
+            var programaActual = db.RadioPrograms
+                .Include(p => p.ProgramDays)
+                .Include(p => p.ProgramHosts)
+                .Include(p => p.CustomersComments.Select(c => c.Customers))
+                .FirstOrDefault(p => db.ProgramDays
+                    .Any(d => d.ProgramId == p.ProgramId && d.WeekDay == currentDay) &&
+                    p.StartTime <= currentTime && p.EndTime >= currentTime);
+
+
+            int? idProgramaActual = programaActual?.ProgramId;
+
+            var todosMenosElActual = db.RadioPrograms
+                .Where(p => idProgramaActual == null || p.ProgramId != idProgramaActual)
+                .OrderByDescending(p => p.StartTime)
+                .ToList();
+
+
+
+
+            var model = new RadioProgramsViewModel
+            {
+                ListaRadios = todosMenosElActual,
+                RadioActual = programaActual,
+                Conductores = programaActual?.ProgramHosts
+                     .Select(ph => ph.Hosts)
+                     .ToList() ?? new List<Hosts>()
+
+            };
+
+            return View(model);
+        }
+
+
+
+
+
+
+
+        // GET: RadioPrograms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RadioPrograms radioPrograms = db.RadioPrograms.Find(id);
+            if (radioPrograms == null)
+            {
+                return HttpNotFound();
+            }
+            return View(radioPrograms);
+        }
+
+        // GET: RadioPrograms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RadioPrograms/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProgramId,ProgramName,Image,Description,StartTime,EndTime")] RadioPrograms radioPrograms)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RadioPrograms.Add(radioPrograms);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(radioPrograms);
+        }
+
+        // GET: RadioPrograms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RadioPrograms radioPrograms = db.RadioPrograms.Find(id);
+            if (radioPrograms == null)
+            {
+                return HttpNotFound();
+            }
+            return View(radioPrograms);
+        }
+
+        // POST: RadioPrograms/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProgramId,ProgramName,Image,Description,StartTime,EndTime")] RadioPrograms radioPrograms)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(radioPrograms).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(radioPrograms);
+        }
+
+        // GET: RadioPrograms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RadioPrograms radioPrograms = db.RadioPrograms.Find(id);
+            if (radioPrograms == null)
+            {
+                return HttpNotFound();
+            }
+            return View(radioPrograms);
+        }
+
+        // POST: RadioPrograms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RadioPrograms radioPrograms = db.RadioPrograms.Find(id);
+            db.RadioPrograms.Remove(radioPrograms);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
