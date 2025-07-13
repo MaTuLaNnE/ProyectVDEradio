141e3f1536882dd15c7264fbb869cec2e0b3afdf
MaTuLaNnE <matias.lanne@estudiantes.utec.edu.uy>
2025-07-13 14:32:42 -0300

    CRUD de Permisos agregado Fix de DeletedConfirmed

---
+++ ProyectVDEradio/ViewModels/PermissionRoleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectVDEradio.ViewModels
+{
+    public class PermissionRoleViewModel
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public List<PermissionCheckbox> Permisos { get; set; }
+    }
+
+    public class PermissionCheckbox
+    {
+        public int PermissionId { get; set; }
+        public string PermissionName { get; set; }
+        public bool EstaAsignado { get; set; }
+    }
+}
